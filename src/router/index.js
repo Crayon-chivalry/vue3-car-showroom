e92dadf354641c15b7671c2a02d139020ae73cc0
123 <1193838563@qq.com>
2025-05-05 13:56:14 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/dashboard',
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('@/views/dashboard/index.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
